d169af108471930287f1cec0144ad1d6b5e6d952
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-09-21 15:26:14 +0300

    Add methods for management app

---
+++ src/main/java/org/example/service/AppService.java
@@ -0,0 +1,67 @@
+package org.example.service;
+
+import org.example.controller.UserCreateController;
+import org.example.controller.UserDeleteController;
+import org.example.controller.UserReadController;
+import org.example.exaptions.OptionException;
+import org.example.repository.UserCreateRepository;
+import org.example.repository.UserDeleteRepository;
+import org.example.repository.UserReadRepository;
+import org.example.utils.AppStarter;
+import org.example.utils.Constants;
+import org.example.view.UserCreateView;
+import org.example.view.UserDeleteView;
+import org.example.view.UserReadView;
+
+public class AppService {
+    public void createUser() {
+        UserCreateRepository repository = new UserCreateRepository();
+        UserCreateService service = new UserCreateService(repository);
+        UserCreateView view = new UserCreateView();
+        UserCreateController controller = new UserCreateController(service, view);
+        controller.createUser();
+    }
+
+    public void readUser() {
+        UserReadRepository repository = new UserReadRepository();
+        UserReadService service = new UserReadService(repository);
+        UserReadView view = new UserReadView();
+        UserReadController controller = new UserReadController(service, view);
+        controller.readUser();
+    }
+
+//
+
+    public void deleteUser() {
+        UserDeleteRepository repository = new UserDeleteRepository();
+        UserDeleteService service = new UserDeleteService(repository);
+        UserDeleteView view = new UserDeleteView();
+        UserDeleteController controller = new UserDeleteController(service, view);
+        controller.deleteUser();
+    }
+
+    public void getNoSuchOption(int choice) {
+        int[] menuChoices = {0, 1, 2, 3, 4};
+        if (!contains(menuChoices, choice)) {
+            try {
+                throw new OptionException(Constants.INCORRECT_VALUE_MSG);
+            } catch (OptionException e) {
+                System.out.println(e.getMessage());
+                AppStarter.startApp();
+            }
+        }
+    }
+
+    // Проверка наличия ввода в массиве выбора
+    public static boolean contains(final int[] options, final int value) {
+        boolean result = false;
+        for (int i : options) {
+            if (i == value) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+}
+
