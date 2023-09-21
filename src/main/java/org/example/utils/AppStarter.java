16bfedcaf1d5c9d20cee29238b103059734c690f
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-09-21 16:20:59 +0300

    Add method startApp

--- src/main/java/org/example/utils/AppStarter.java
+++ src/main/java/org/example/utils/AppStarter.java
@@ -1,11 +1,15 @@
 package org.example.utils;
 
+import org.example.controller.AppController;
+import org.example.controller.AppControllerUpdate;
+import org.example.service.AppService;
 import org.example.view.AppView;
+import org.example.view.AppViewUpdate;
 
 public class AppStarter {
     public static void startApp(){
         AppService service = new AppService();
-        AppViewUpd viewUp = new AppViewUpd();
+        AppViewUpdate viewUp = new AppViewUpdate();
         AppView view = new AppView();
         AppControllerUpdate appControllerUpdate = new AppControllerUpdate(viewUp, service);
         AppController controller = new AppController(view, service);
