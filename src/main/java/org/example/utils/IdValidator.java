33e5f3c5744484f50bf6b3512525cdbbcb57d02b
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-09-21 14:57:23 +0300

    Rename class

--- src/main/java/org/example/utils/IdValidator.java
+++ src/main/java/org/example/utils/IdValidator.java
@@ -1,6 +1,6 @@
 package org.example.utils;
 
-public class isIdValid {
+public class IdValidator {
     public static boolean isIdValid(String id) {
         return id.isEmpty() || !id.matches(Constants.ID_RGX);
     }
