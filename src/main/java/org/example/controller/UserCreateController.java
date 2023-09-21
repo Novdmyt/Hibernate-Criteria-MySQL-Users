ea8e9eb5984bcb29d2a019e21de39b4afeff2e43
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-09-21 15:09:12 +0300

    Add method createUser

---
+++ src/main/java/org/example/controller/UserCreateController.java
@@ -0,0 +1,19 @@
+package org.example.controller;
+
+import org.example.service.UserCreateService;
+import org.example.view.UserCreateView;
+
+public class UserCreateController {
+    UserCreateService service;
+    UserCreateView view;
+
+    public UserCreateController(UserCreateService service, UserCreateView view){
+        this.service = service;
+        this.view = view;
+    }
+
+    public void createUser() {
+        view.getOutput(service.userContact(view.getData()));
+        AppStarter.startApp();
+    }
+}
