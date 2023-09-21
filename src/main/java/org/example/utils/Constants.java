cc3c9311dac726a78842a70eae00853a03971dce
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-09-21 21:37:59 +0300

    Fix code

--- src/main/java/org/example/utils/Constants.java
+++ src/main/java/org/example/utils/Constants.java
@@ -7,16 +7,15 @@ public class Constants {
     public final static String DB_USER = "dev";
     public final static String EMAIL_RGX = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
     public final static String WRONG_EMAIL_MSG = "Wrong email input.";
-    public final static String WRONG_LAST_NAME = "Wrong Last Name input.";
+    public final static String WRONG_NAME = "Wrong input.";
     public final static String ID_RGX = "^[1-9]$";
-    public final static String LAST_NAME = "^[a-zA-Z0-9]$";
+    public final static String LAST_NAME = "^[a-zA-Z0-9_ -]+$";
     public final static String WRONG_ID_MSG = "Wrong id input.";
     public final static String DATA_ABSENT_MSG = "\n>> No data!";
     public final static String DATA_INSERT_MSG = "\n>> Created.";
     public final static String DATA_UPDATE_MSG = "\n>> Updated.";
     public final static String DATA_DELETE_MSG = "\n>> Deleted.";
     public final static String APP_CLOSE_MSG = "\n>> App closed.";
-    public final static String INPUT_REQ_MSG = "Input required.";
     public final static String INCORRECT_VALUE_MSG = ">> Incorrect value! Try again.";
     public final static String ID_NO_EXISTS_MSG = "\n>> There is no such ID.";
 }
--- src/main/java/org/example/utils/NameValidator.java
+++ src/main/java/org/example/utils/NameValidator.java
@@ -1,6 +1,6 @@
 package org.example.utils;
 
-public class LastNameValidator {
+public class NameValidator {
     public static boolean isLastNameValidator(String lastName){
 
         return lastName.isEmpty()|| !lastName.matches(Constants.LAST_NAME);
